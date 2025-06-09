03004b4036e00d4a34956098eb2d2e9e332abc65
Bilal Hantik <bilalhantik@gmail.com>
2025-06-09 22:07:43 +0300

    Add files via upload

---
+++ SQL Ödev 5.sql
@@ -0,0 +1,18 @@
+-- Ödev 5 1. SORU
+
+--SELECT*FROM film WHERE title LIKE '%n'
+--ORDER BY length DESC 
+--LIMIT 5;
+
+-- Ödev 5 2. Soru
+
+--SELECT*FROM film WHERE title LIKE '%n'
+--ORDER BY length DESC 
+--OFFSET 6
+--LIMIT 5;
+
+-- Ödev 5 3. Soru
+
+--SELECT * FROM customer WHERE store_id = 1
+--ORDER BY last_name DESC
+--LIMIT 4;
