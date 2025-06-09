0ee53d6c8beb68c70dfad3c4afa94064f32b392d
Bilal Hantik <bilalhantik@gmail.com>
2025-06-09 22:23:21 +0300

    Add files via upload

---
+++ SQL Ödev 6.sql
@@ -0,0 +1,18 @@
+--Ödev 6 Soru 1: 
+-- AVG ile ortalamayı alıyoruz. 
+--Round ile 0'dan sonra kaç değer yazılacağını belirtiyoruz.
+
+--SELECT round(AVG(rental_rate), 2) FROM film ;
+
+
+-- Ödev 6 Soru 2
+--SELECT COUNT(*) FROM film WHERE title LIKE 'C%';
+
+-- Ödev 6 Soru 3
+
+--SELECT MAX(length) FROM film WHERE rental_rate = 0.99; 
+
+-- Ödev 6 Soru 4
+
+--SELECT COUNT(DISTINCT replacement_cost) FROM film WHERE length >150 ;
+
